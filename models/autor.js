8023fd067ea48acf0a91bbc3f44aae77a3a92439
Jhosue-Chica <jichica@espe.edu.ec>
2025-03-02 20:11:14 -0500

    ACT 1

--- models/autor.js
+++ models/autor.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Autor extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Autor.hasMany(models.Libro, { foreignKey: 'autorId', as: 'libros' });
     }
--- models/libro.js
+++ models/libro.js
@@ -4,13 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Libro extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
       Libro.belongsTo(models.Autor, { foreignKey: 'autorId', as: 'autor' });
       Libro.belongsTo(models.Genero, { foreignKey: 'generoId', as: 'genero' });
     }
